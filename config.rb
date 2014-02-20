931f6c772a88e8614620fb3a3244fe1b19285804
Stuart Olivera <stuart@stuartolivera.com>
2014-02-20 02:05:12 -0500

    Switched to proper bootstrap compass gem

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 
-require 'bootstrap-sass'
+require 'compass_twitter_bootstrap'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
