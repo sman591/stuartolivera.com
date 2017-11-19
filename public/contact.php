535c35c79fdb7627e3f5386fa6397776989f10f6
Stuart Olivera <stuart@stuartolivera.com>
2017-11-19 18:20:14 -0500

    Use SparkPost for email transmission

--- public/contact.php
+++ public/contact.php
@@ -17,6 +17,40 @@ if (trim($subject) != '')
   $content .= "Subject:  " . $subject . "\r\n";
 $content .=  "Message:  \r\n" . $message . "\r\n----";
 
-mail('stuart@oliveraweb.com', 'Contact From ' . $_SERVER['HTTP_HOST'], $content);
+require '../vendor/autoload.php';
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+
+$httpClient = new GuzzleAdapter(new Client());
+$sparky = new SparkPost($httpClient, ['key'=>getenv('SPARKPOST_API_KEY')]);
+
+$promise = $sparky->transmissions->post([
+    'content' => [
+        'from' => [
+            'name' => 'stuartolivera.com',
+            'email' => 'stuart@oliveraweb.com',
+        ],
+        'subject' => 'Contact From stuartolivera.com',
+        'text' => $content
+    ],
+    'recipients' => [
+        [
+            'address' => [
+                'name' => 'Stuart Olivera',
+                'email' => 'stuart@stuartolivera.com',
+            ],
+        ],
+    ],
+]);
+
+try {
+    $response = $promise->wait();
+    echo $response->getStatusCode()."\n";
+} catch (\Exception $e) {
+    http_response_code(500);
+    echo $e->getCode()."\n";
+    echo $e->getMessage()."\n";
+}
 
 ?>
